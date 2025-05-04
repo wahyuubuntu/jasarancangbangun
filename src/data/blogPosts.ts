2b024b9d1c23256bca151ba158629cc4d398779a
Masbro <wahyuubuntu@gmail.com>
2025-05-04 14:34:12 +0700

    Update blogPosts.ts

--- src/data/blogPosts.ts
+++ src/data/blogPosts.ts
@@ -3,11 +3,11 @@ export const blogPosts = [
   {
     id: 1,
     title: "Tips Memilih Kontraktor Terpercaya untuk Proyek Anda",
-    excerpt: "Memilih kontraktor yang tepat adalah langkah penting dalam kesuksesan proyek konstruksi. Artikel ini membahas beberapa tips penting yang perlu dipertimbangkan saat memilih kontraktor.",
+    excerpt: "IMB (Izin Mendirikan Bangunan) adalah izin yang dikeluarkan oleh pemerintah daerah (kabupaten/kota) kepada pemilik bangunan untuk membangun, mengubah, memperluas, mengurangi, atau merawat bangunan gedung sesuai dengan standar teknis yang berlaku. IMB ini merupakan bentuk persetujuan dan perizinan yang wajib dimiliki oleh pemilik bangunan untuk memastikan bangunan yang didirikan aman dan sesuai dengan peruntukan lahan. ",
     date: "12 April 2025",
     category: "Tips & Saran",
-    readTime: "5 menit",
-    image: "https://images.unsplash.com/photo-1541888946425-d81bb19240f5?q=80&w=870"
+    readTime: "2 menit",
+    image: "https://mmc.kalteng.go.id/files/berita/29112019031945_0.jpeg"
   },
   {
     id: 2,
@@ -15,7 +15,7 @@ export const blogPosts = [
     excerpt: "Ketahui tren desain rumah terbaru yang populer di Indonesia tahun ini, dari konsep minimalis hingga sentuhan tradisional modern yang menarik.",
     date: "28 Maret 2025",
     category: "Desain",
-    readTime: "8 menit",
+    readTime: "2 menit",
     image: "https://images.unsplash.com/photo-1613545325278-f24b0cae1224?q=80&w=1170"
   },
   {
@@ -24,7 +24,7 @@ export const blogPosts = [
     excerpt: "Eksplorasi berbagai pilihan material konstruksi ramah lingkungan yang dapat digunakan untuk membangun proyek yang lebih berkelanjutan dan hemat energi.",
     date: "15 Maret 2025",
     category: "Material",
-    readTime: "6 menit",
+    readTime: "1 menit",
     image: "https://images.unsplash.com/photo-1607472586893-edb57bdc0e39?q=80&w=1974"
   },
   {
@@ -33,7 +33,7 @@ export const blogPosts = [
     excerpt: "Panduan praktis untuk mengelola anggaran proyek renovasi rumah agar tetap dalam batas dan menghindari pengeluaran tak terduga.",
     date: "2 Maret 2025",
     category: "Manajemen",
-    readTime: "7 menit",
+    readTime: "2,5 menit",
     image: "https://images.unsplash.com/photo-1628254095259-cd352547a0af?q=80&w=1170"
   },
   {
@@ -42,7 +42,7 @@ export const blogPosts = [
     excerpt: "Ide-ide kreatif untuk memaksimalkan ruang terbatas dengan desain interior yang fungsional namun tetap estetis dan nyaman.",
     date: "18 Februari 2025",
     category: "Interior",
-    readTime: "5 menit",
+    readTime: "2 menit",
     image: "https://images.unsplash.com/photo-1618220179428-22790b461013?q=80&w=1227"
   },
 ];
